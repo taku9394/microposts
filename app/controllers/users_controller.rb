eb010ae0b0706aa3e59e7b376ef9e8a7edd4c16c
Takuro Sawaguchi <takuro-sawaguchi@united.jp>
2016-04-14 15:19:53 +0000

    lesson7_1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,11 +17,24 @@ class UsersController < ApplicationController
     end
   end
   
+ def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+   redirect_to root_path , notice: 'プロフィールを編集しました'
+    else
+      render 'edit'
+    end
+  end
+
+  
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
   
 end
