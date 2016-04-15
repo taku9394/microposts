fcd4d60284bf366490f0da0991409c846b7d5110
Takuro Sawaguchi <takuro-sawaguchi@united.jp>
2016-04-15 10:50:38 +0000

    lesson7_5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  
+  before_action :logged_in_user, only: [:edit, :update]
+   before_action :correct_user,   only: [:edit, :update]#これ追加！
+   
   def show # 追加
    @user = User.find(params[:id])
   end
@@ -25,7 +27,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-   redirect_to root_path , notice: 'プロフィールを編集しました'
+   redirect_to @user , notice: 'プロフィールを編集しました'
     else
       render 'edit'
     end
@@ -35,7 +37,23 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :area, :email, :password, :password_confirmation)
   end
+  
+  # beforeフィルター
+
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
   
 end
