7c908747c80061e0011543e04b1d9375f091d6c3
Takuro Sawaguchi <takuro-sawaguchi@united.jp>
2016-04-16 06:22:23 +0000

    add posting microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,9 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
    before_action :correct_user,   only: [:edit, :update]#これ追加！
    
-  def show # 追加
-   @user = User.find(params[:id])
+  def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.order(created_at: :desc)
   end
   
   def new
