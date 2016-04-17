2a3ead93ceeab21870af1c13aedc09e798db198a
Takuro Sawaguchi <takuro-sawaguchi@united.jp>
2016-04-17 01:22:49 +0000

    lesson7_8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
-   before_action :correct_user,   only: [:edit, :update]#これ追加！
+    before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :followings, :followers]
+    before_action :correct_user,   only: [:edit, :update]
    
   def show
     @user = User.find(params[:id])
@@ -34,6 +34,17 @@ class UsersController < ApplicationController
     end
   end
 
+  def followings
+    @title = "Following"
+    @users = @user.following_users.paginate(page: params[:page], :per_page => 5)
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @users = @user.follower_users.paginate(page: params[:page], :per_page => 5)
+    render 'show_follow'
+  end
   
   private
 
@@ -56,5 +67,8 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
     end
+    
+  
+
   
 end
